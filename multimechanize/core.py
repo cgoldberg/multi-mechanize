c2bc070d83dc6712248d309dc327d6f7f3ae1546
Corey Goldberg <cgoldberg@gmail.com>
2012-01-21 12:36:00 -0500

    changed default directory to current dir. updated copyright headers

--- multimechanize/core.py
+++ multimechanize/core.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-#  Copyright (c) 2010-2011 Corey Goldberg (corey@goldb.org)
+#  Copyright (c) 2010-2012 Corey Goldberg (corey@goldb.org)
 #  License: GNU LGPLv3
 #  
 #  This file is part of Multi-Mechanize
